83cbf80ffae1c5b465af9e5f0443ea04574e7b33
Flora Harvey <fharvey555@gmail.com>
2017-02-28 11:14:41 +0000

    Adds form

--- lib/app.rb
+++ lib/app.rb
@@ -13,8 +13,13 @@ get '/fact' do
   "Let me tell you a fact..."
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb :index
+end
 
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
 end
